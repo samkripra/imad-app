e865d45332da0ec2065b1a4f3f3b8ced12568e7b
SAMEER PRADHAN <30686684+samkripra@users.noreply.github.com>
2017-08-07 18:24:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req,res)
-{res.send('Article one requested and will be served here');
+{res.send('Article one/1 requested and will be served here');
     
 });
 app.get('/article-two', function(req,res)
