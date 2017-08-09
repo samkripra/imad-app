8fe2815742bb374a31553f500012a6131e31e65e
SAMEER PRADHAN <30686684+samkripra@users.noreply.github.com>
2017-08-09 09:09:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 1;
+app.get('/counter', function(req,res){
+counter = counter+1;
+res.send(counter,toString())
+}
+
 app.get('/article-one', function(req,res)
 {res.send('Article one/1 requested and will be served here');
     
